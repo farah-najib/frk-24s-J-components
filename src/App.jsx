7aaa90da6374352c778e9e7375efd7a261c09d43
julsandstrom <juliiansandstrom@gmail.com>
2025-09-16 10:55:48 +0200

    cleaning up components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import DragMenuBtn from "./components/DragMenuBtn";
 import Button from "./components/Button";
 import MenuModal from "./components/MenuModal";
+import Grid from "./components/Grid";
 import { useState } from "react";
 function App() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
+      <Grid />
       {/* Draggable menu button */}
       {!showModal && <DragMenuBtn onClick={() => setShowModal(true)} />}
       {showModal && <MenuModal setShowModal={setShowModal} />}
