e883bf078f7c0dcedf0d2cb2d07fd179887c1e91
julsandstrom <juliiansandstrom@gmail.com>
2025-09-16 13:55:59 +0200

    removed boilerplate css

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
 import "./App.css";
-//import Player from "./components/Player/Player";
-//import DragMenuBtn from "./components/DragMenuBtn";
-//import Button from "./components/Button";
-//import MenuModal from "./components/MenuModal";
-//import Grid from "./components/Grid";
-import { useState } from "react";
+import Board from "./components/Board/Board";
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
   return (
     <>
-      <Grid />
-      {/* Draggable menu button */}
-      {!showModal && <DragMenuBtn onClick={() => setShowModal(true)} />}
-      {showModal && <MenuModal setShowModal={setShowModal} />}
-      <br />
-      <br />
-
-      {/* Custom reusable button */}
-      <Button variant="destructive" onClick={() => alert("hello")}>
-        TEST BUTTON
-      </Button>
-	  <Player />
+      <Board />
     </>
   );
 }
