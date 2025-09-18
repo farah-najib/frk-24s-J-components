969eb47b520c21fc19d4173c00851824f9a2fd53
julsandstrom <juliiansandstrom@gmail.com>
2025-09-18 14:44:15 +0200

    Input component with simple sanitizer

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,30 +1,31 @@
 const Player = ({name, number}) => {
 
-	let fallbackName = name;
-	let fallbackNumber = number;
+	let safeName = name;
+	let safeNumber = number;
 
 	  if(typeof(number) !== "number" || number <= 0){
                 console.error(`Invalid number type: ${typeof(number)}. Only positive numbers are allowed.`)
-                fallbackNumber = 99;
+                safeNumber = 99;
+		  
         }
 
 
 	if (typeof(name) !== "string"){
 		console.error(`Invalid name type: ${typeof(name)}. Only string is allowed.`)
-		fallbackName = "Guest"
+		safeName = "Guest"
 
 	} else if(!/^[A-Za-z]+$/.test(name)){
 		console.error(`Invalid characters in name: ${name}. Only letters allowed.`)
-		fallbackName = "Guest"
+		safeName = "Guest"
 
 	}else if (name.length < 2 || name.length > 15){
 		console.error(`Invalid name length: ${name.length}. Should be 2-15 chars.`)
-		fallbackName = "Guest"
+		safeName = "Guest"
 	}
 
 
 	return(
-		<p>Player {fallbackNumber}: {fallbackName}</p>
+		<p>Player {safeNumber}: {safeName}</p>
 )
 }
 
