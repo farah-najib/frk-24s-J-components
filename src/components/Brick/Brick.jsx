c2e3ba1aeddb4553c7a2eb707abf8b38c60ba8a8
julsandstrom <juliiansandstrom@gmail.com>
2025-09-18 13:10:47 +0200

    added fallback for error on Brick.jsx

--- src/components/Brick/Brick.jsx
+++ src/components/Brick/Brick.jsx
@@ -3,6 +3,11 @@ export const Brick = ({color="black"}) => {
 	 
 	const allowedColors = ["black", "white"];
 
-	if(!allowedColors.includes(color)){return <>invalid color</>}
+	if(!allowedColors.includes(color)){
+		console.error(`Brick recieved invalid color: ${color}`);
+
+		return <div className={`${styles.brick} ${styles.black}`} />
+
+	}
 	
 	return <div className={`${styles.brick} ${color === "black" ? styles.black : styles.white}`}> </div>}; export default Brick;
