1bb29d35fe70ffb493515c4bd2862575f17872ae
juliansandstrom <juliiansandstrom@gmail.com>
2025-09-22 13:29:49 +0200

    pairprogramming school mob

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,7 +1,31 @@
 import styles from './Board.module.css'
+import Cell from "../Cell/Cell.jsx"
+
+
+const boardTiles = 30;
 
 const Board = () => {
-    return <div className={styles.board}></div>
+		
+		
+		
+	let cellsToRender = [];
+
+	for(let i = 0; i <= 80; i++){
+		cellsToRender.push(i);
+	}
+
+
+
+
+    return <div className={styles.board}>
+		{cellsToRender.map((cell, idx) => (
+			
+		<Cell key={idx} />
+
+		))}
+
+
+		</div>
 }
 export default Board
 
