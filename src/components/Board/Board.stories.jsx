b69b58753dd7beb4d6833087286a5f5020227467
Max Welin <maxwwelin@gmail.com>
2025-09-26 10:22:30 +0200

    Updated all Board stories to now take a gameConfig object as prop

--- src/components/Board/Board.stories.jsx
+++ src/components/Board/Board.stories.jsx
@@ -7,40 +7,52 @@ export default {
 
 export const Default = {
   args: {
-    boardTiles: 80,
-    gameStarted: true,
+    gameConfig: {
+      boardTiles: 80,
+      gameStarted: true,
+    }
   },
 };
 
 export const Player = {
   render: () => (
-    <Board gameStarted={true} boardTiles={80} playerOrder={"Player 1"} />
+    <Board gameConfig={{
+      gameStarted: true,
+      boardTiles: 80,
+      playerOrder: "Player 1"
+    }} />
   ),
 };
 
 export const StartMenu = {
   render: () => (
-    <Board
-      gameStarted={false}
-      boardTiles={80}
-      playerOrder={"Player 1"}
-      largeMenu={false}
+    <Board gameConfig={{
+      gameStarted: false,
+      boardTiles: 80,
+      playerOrder: "Player 1",
+      largeMenu: false
+    }} 
     />
   ),
 };
 export const MidGameMenu = {
   render: () => (
-    <Board gameStarted={false} boardTiles={80} playerOrder={"Player 1"} />
+    <Board gameConfig={{
+      gameStarted: false,
+      boardTiles: 80,
+      playerOrder: "Player 1"
+    }} />
   ),
 };
 
 export const GameOver = {
   render: () => (
-    <Board
-      gameStarted={true}
-      boardTiles={80}
-      playerOrder={"Player 1"}
-      gameEnded={true}
+    <Board gameConfig={{
+      gameStarted: true,
+      boardTiles: 80,
+      playerOrder: "Player 1",
+      gameEnded: true
+    }}
     />
   ),
 };
