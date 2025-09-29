01eb9259c4e3ac549d7435e194ec40d6212239ca
juliansandstrom <juliiansandstrom@gmail.com>
2025-09-29 14:13:18 +0200

    props updated in Board.jsx

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,19 +3,15 @@ import Cell from "../Cell/Cell.jsx";
 import Menu from "../Menu";
 import VictoryScreen from "../VictoryScreen";
 
-
-const Board = ({ gameConfig, handleCellClick, playerTurn }) => {
-  
-  const {
+const Board = ({
+  playerTurn,
+  handleCellClick,
   boardTiles,
   className = "",
-  gameStarted,
-  largeMenu,
-  gameEnded
-} = gameConfig
-
-  
-  
+  gameStarted = true,
+  largeMenu = true,
+  gameEnded = false,
+}) => {
   let cellsToRender = [];
 
   for (let i = 0; i <= boardTiles; i++) {
@@ -25,34 +21,38 @@ const Board = ({ gameConfig, handleCellClick, playerTurn }) => {
   const currentColor = playerTurn === 1 ? "black" : "white";
 
   return (
- <div className={styles.boardWrapper}>
-    <div className={`${styles.board} ${className}`} onClick={handleCellClick}>
-      {/* Player story - text behind cells */}
-      {!gameEnded && (
-        <div className={styles.playerOverlay}>Player {playerTurn}</div>
-      )}
+    <div className={styles.boardWrapper}>
+      <div className={`${styles.board} ${className}`} onClick={handleCellClick}>
+        {/* Player story - text behind cells */}
+        {!gameEnded && (
+          <div className={styles.playerOverlay}>Player {playerTurn}</div>
+        )}
 
-      <div className={styles.cells}>
-        {cellsToRender.map((_, idx) => (
-          <Cell key={idx} disabled={!gameStarted} initialColor={currentColor} />
-        ))}
+        <div className={styles.cells}>
+          {cellsToRender.map((_, idx) => (
+            <Cell
+              key={idx}
+              disabled={!gameStarted}
+              initialColor={currentColor}
+            />
+          ))}
+        </div>
+
+        {/* Mid Game Menu story */}
+        {!gameStarted && !gameEnded && (
+          <div className={styles.menu}>
+            <Menu startGame={largeMenu} />{" "}
+          </div>
+        )}
+
+        {/* Victory Screen story */}
+        {gameEnded && gameStarted && (
+          <div className={styles.scoreScreen}>
+            <VictoryScreen />
+          </div>
+        )}
       </div>
-
-      {/* Mid Game Menu story */}
-      {!gameStarted && !gameEnded && (
-        <div className={styles.menu}>
-          <Menu startGame={largeMenu} />{" "}
-        </div>
-      )}
-
-      {/* Victory Screen story */}
-      {gameEnded && gameStarted && (
-        <div className={styles.scoreScreen}>
-          <VictoryScreen />
-        </div>
-      )}
- </div>
- </div>
+    </div>
   );
-  };
+};
 export default Board;
