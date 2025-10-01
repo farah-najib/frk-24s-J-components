a8b2b680ff5885579512751bd45d4f501be6b6be
1anDr0 <jerker_danielsson@hotmail.com>
2025-10-01 13:55:38 +0200

    fixed style on brick

--- src/components/BackgroundBanner/BackgroundBanner.stories.jsx
+++ src/components/BackgroundBanner/BackgroundBanner.stories.jsx
@@ -3,26 +3,22 @@ import BackgroundBanner from "./BackgroundBanner";
 export default {
   title: "components/BackgroundBanner",
   component: BackgroundBanner,
+  args: { text: "GOMOKU", playerTurn: undefined, useTurnText: false },
+  argTypes: {
+    playerTurn: { control: { type: "radio" }, options: [1, 2] },
+    useTurnText: { control: "boolean" },
+  },
 };
 
 export const Default = {};
 
-export const Winner = {
-  args: { text: "WINNER" },
-};
+export const Winner = { args: { text: "WINNER" } };
 
-export const White = {
-  args: { text: "WHITE", playerTurn: 2 },
-};
+export const TurnBlack = { args: { playerTurn: 1, useTurnText: true } };
+export const TurnWhite = { args: { playerTurn: 2, useTurnText: true } };
 
-export const Black = {
-  args: { text: "BLACK", playerTurn: 1 },
-};
+export const White = { args: { text: "WHITE", playerTurn: 2 } };
+export const Black = { args: { text: "BLACK", playerTurn: 1 } };
 
-export const Error404 = {
-  args: { text: "404" },
-};
-
-export const BreakMyComponent = {
-  args: { text: "12345" },
-};
+export const Error404 = { args: { text: "404" } };
+export const BreakMyComponent = { args: { text: "12345" } };
