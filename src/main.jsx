e908f0a2aadf021a95ea5dcb073fb6dfaaac1859
1anDr0 <jerker_danielsson@hotmail.com>
2025-10-01 11:21:29 +0200

    funct to backgroundbanner

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
