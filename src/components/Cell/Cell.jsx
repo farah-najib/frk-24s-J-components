8257272a09e414f444bc764cf478a89d27e87ed8
juliansandstrom <juliiansandstrom@gmail.com>
2025-10-02 14:19:50 +0200

    removed story related crap in cell

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -2,52 +2,8 @@ import styles from "./Cell.module.css";
 import { useState, useRef, useEffect } from "react";
 import Brick from "../Brick/Brick.jsx";
 
-export default function Cell({
-  initialColor = "black",
-  isActivated = false,
-  onActivate,
-  handleCellClick,
-  cellIndex,
-  brickColor,
-}) {
-  const safeColor = (() => {
-    const x = String(initialColor).toLowerCase();
-    if (x === "black" || x === "white") return x;
-    console.warn(`Invalid Color:${initialColor}. Defaulting to "black".`);
-    return "black";
-  })();
-
-  const safeActive =
-    typeof isActivated === "boolean"
-      ? isActivated
-      : (() => {
-          console.warn("Is not a boolean");
-          return Boolean(isActivated);
-        })();
-
-  const [active, setActive] = useState(isActivated);
-  const [color, setColor] = useState(safeColor);
-
-  useEffect(() => {
-    setActive(safeActive);
-  }, [safeActive]);
-
-  useEffect(() => {
-    if (!active) setColor(safeColor);
-  }, [safeColor]);
-
-  const handleClick = () => {
-    if (!active) {
-      setActive(true);
-      setColor(safeColor);
-      handleCellClick();
-      onActivate?.(safeColor);
-    }
-
-    setTimeout(() => {
-      clickLock.current = false;
-    }, 0);
-  };
+export default function Cell({ handleCellClick, cellIndex, brickColor }) {
+  const [active, setActive] = useState(true);
 
   return (
     <>
@@ -55,8 +11,9 @@ export default function Cell({
         type="button"
         onClick={() => handleCellClick(cellIndex)}
         className={`${styles.cell} ${active && styles.active}`}
+        aria-pressed={active}
       >
-        {active && <Brick color={brickColor} aria-pressed={active} />}
+        {active && <Brick color={brickColor} />}
       </button>
     </>
   );
