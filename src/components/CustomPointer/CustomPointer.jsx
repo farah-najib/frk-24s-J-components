ce6b61c42304dc529290bc9b7732b1e9f180e36a
1anDr0 <jerker_danielsson@hotmail.com>
2025-10-06 13:22:52 +0200

    working custompointer

--- src/components/CustomPointer/CustomPointer.jsx
+++ src/components/CustomPointer/CustomPointer.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import styles from "./CustomPointer.module.css";
 
-export default function CustomPointer({ color = "black" }) {
+export default function CustomPointer({ playerTurn = 1 }) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const color = playerTurn === 1 ? "black" : "white";
+
+  useEffect(() => {
+    const handleMove = (e) => setPosition({ x: e.clientX, y: e.clientY });
+    window.addEventListener("mousemove", handleMove);
+    return () => window.removeEventListener("mousemove", handleMove);
+  }, []);
+
   return (
     <div
-      className={`${styles.circle} ${
-        color === "black" ? styles.black : styles.white
-      }`}
+      className={`${styles.circle} ${styles[color]}`}
+      style={{ left: `${position.x}px`, top: `${position.y}px` }}
     />
   );
 }
