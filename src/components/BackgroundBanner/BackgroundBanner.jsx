539e66708d42d1a9dd0259d238a4a9db2bd7ebd4
Max Welin <maxwwelin@gmail.com>
2025-10-06 14:18:57 +0200

    backgroundbanner now takes players as props and display player name, defaults to black / white if no name found

--- src/components/BackgroundBanner/BackgroundBanner.jsx
+++ src/components/BackgroundBanner/BackgroundBanner.jsx
@@ -2,20 +2,23 @@ import styles from "./BackgroundBanner.module.css";
 
 const BackgroundBanner = ({
   text = "GOMOKU",
-  className = "",
   playerTurn,
   useTurnText = false,
+  players
 }) => {
   const isValidTurn = playerTurn === 1 || playerTurn === 2;
   const isP1 = playerTurn === 1;
 
+  const p1Name = players[1].name.toUpperCase()
+  const p2Name = players[2].name.toUpperCase()
+
   const label =
-    useTurnText && isValidTurn ? (isP1 ? "BLACK" : "WHITE") : text ?? "GOMOKU";
+    useTurnText && isValidTurn ? (isP1 ? p1Name || "BLACK" :  p2Name || "WHITE") : text ?? "GOMOKU";
 
   const inlineStyle = isValidTurn ? { color: isP1 ? "black" : "white" } : {};
 
   return (
-    <div className={`${styles.banner} ${className}`}>
+    <div className={`${styles.banner}`}>
       <span className={styles.text} style={inlineStyle}>
         {label}
       </span>
