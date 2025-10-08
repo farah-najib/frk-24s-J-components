5418c1b3798a7a8e3840c9f3408e80f8e2ce0046
juliansandstrom <juliiansandstrom@gmail.com>
2025-10-08 10:00:17 +0200

    fix: remove timer

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,24 +1,21 @@
-import styles from "./Board.module.css";
-import Cell from "../Cell/Cell.jsx";
-import Menu from "../Menu";
-import VictoryScreen from "../VictoryScreen";
-import Timer from "../Timer/Timer.jsx";
+import styles from './Board.module.css';
+import Cell from '../Cell/Cell.jsx';
 
 const Board = ({ gameState, playerTurn, placeMove, boardRows, boardCols }) => {
   const cols = Array.from({ length: boardCols }, (_, i) => i);
   const rows = Array.from({ length: boardRows }, (_, i) => i);
 
-  const currentColor = playerTurn === 1 ? "black" : "white";
+  const currentColor = playerTurn === 1 ? 'black' : 'white';
 
   return (
     <div className={styles.boardWrapper}>
-       <div className={styles.topBar}>
+      {/* <div className={styles.topBar}>
 	  <Timer initialTime={300} onTimeUp={() => {
 		  alert("Time is up! Restarting game...")
 		  window.location.reload();
 	  }}
 	  />
-	  </div> 
+	  </div>  */}
 
       <div className={styles.board}>
         <div className={styles.cells}>
